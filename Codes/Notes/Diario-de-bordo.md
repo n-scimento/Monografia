adfbd03911a1d1cf406c7ba048eac17e040794ca
n-scimento <g.nascimento.p@icloud.com>
2024-07-24 22:37:26 -0300

    oop

--- Codes/Notes/Diario-de-bordo.md
+++ Codes/Notes/Diario-de-bordo.md
@@ -1,17 +1,17 @@
 # Diário de bordo
 
 ## 2024-07-24
-- [ ] + Outline (acompanhar funções)
-- [ ] + Profiler (tempo de execução dos códigos)
-- [ ] + Project (define o projeto em que estou trabalhando)
-- [ ] + Online Help (help, mas mais robusto)
-- [ ] + Find (achar funções)
-- [ ] + Code Analysis (boas práticas)
+- [x] + Outline (acompanhar funções)
+- [x] + Profiler (tempo de execução dos códigos)
+- [x] + Project (define o projeto em que estou trabalhando)
+- [x] + Online Help (help, mas mais robusto)
+- [x] + Find (achar funções)
+- [x] + Code Analysis (boas práticas)
 - [ ] + Install Spyder Line Profiler (time each line of code takes to run) 
 
 
 ## 2024-07-23
-- [ ] Conseguir puxar as curvas de jurso com uma função e dia 
+- [x] Conseguir puxar as curvas de jurso com uma função e dia 
 - [ ] Conseguir atualizar facilmente a base de dados para o dia mais recente
 - [ ] Conseguir upar facilmente para o Git 
 - [ ] Conseguir plottar gráficos 2D: scattered e line; para qualquer número de séries
--- Codes/Py/bmf.py
+++ Codes/Py/bmf.py
@@ -1,9 +1,21 @@
 import pandas as pd 
 import numpy as np
+import warnings 
+warnings.filterwarnings("ignore")
 
-def real(dt):
-    url = r'https://www2.bmf.com.br/pages/portal/bmfbovespa/boletim1/TxRef1.asp?Data=' + dt.strftime('%d/%m/%Y') + '&slcTaxa=DIC'
-    html = pd.read_html(url) # read B3 data
+#%%# Extração Swap Pré x IPCA
+def _real(dt):
+    """
+    Parameters
+    ----------
+    dt : "dd/mm/yyyy"
+
+    Returns
+    -------
+    data : yield curve for the given date
+    """
+    url = f'https://www2.bmf.com.br/pages/portal/bmfbovespa/boletim1/TxRef1.asp?Data={dt}&slcTaxa=DIC'
+    html = pd.read_html(url, encoding = 'latin1') # read B3 data
     data = None 
 
     if 'Não há dados para a data fornecida!' not in list(html[1].iloc[0]):
@@ -17,12 +29,27 @@ def real(dt):
         
         data = pd.DataFrame(data, columns = ['TenorsDays', 'bd252']) # data cleaning and formatting
         data['Date'] = [dt for n in range(len(data))]
-
+        
+        if(type(data) != type(None)):
+            data = pd.pivot_table(data, values = 'bd252', index = 'Date', columns = ['TenorsDays']).sort_values(by = 'Date') # format new data with pivot table
+            data.columns = data.columns.astype('float64') # format columns 
+            data.index = data.index.astype('datetime64[ns]') # format index (rows)
+            
     return data 
 
-def nominal(dt):
-    url = r'https://www2.bmf.com.br/pages/portal/bmfbovespa/boletim1/TxRef1.asp?Data=' + dt.strftime('%d/%m/%Y') + '&slcTaxa=PRE'
-    html = pd.read_html(url) # read B3 data
+#%%# Extração Swap Pré X DI
+def _nominal(dt):
+    """
+    Parameters
+    ----------
+    dt : "dd/mm/yyyy"
+
+    Returns
+    -------
+    data : yield curve for the given date
+    """
+    url = f'https://www2.bmf.com.br/pages/portal/bmfbovespa/boletim1/TxRef1.asp?Data={dt}&slcTaxa=PRE'
+    html = pd.read_html(url, encoding = 'latin1') # read B3 data
     data = None 
 
     if 'Não há dados para a data fornecida!' not in list(html[1].iloc[0]):
@@ -36,5 +63,71 @@ def nominal(dt):
         
         data = pd.DataFrame(data, columns = ['TenorsDays', 'bd252', 'act360']) # data cleaning and formatting
         data['Date'] = [dt for n in range(len(data))]
-
+        
+        if(type(data) != type(None)):
+            data = pd.pivot_table(data, values = 'bd252', index = 'Date', columns = ['TenorsDays']).sort_values(by = 'Date') # format new data with pivot table
+            data.columns = data.columns.astype('float64') # format columns 
+            data.index = data.index.astype('datetime64[ns]') # format index (rows)
+            
     return data 
+
+#%%# Solicitação da curva Real para um dia ou para um período
+
+def real(start_date, end_date=None): 
+
+    """
+    Parameters
+    ----------
+    start_date: "yyyy-mm-dd"
+    end_date: "yyyy-mm-dd" (optional)
+    
+        
+    Returns
+    -------
+    data: yield curve for the given date or range
+    """
+
+    df = []
+    
+    date_list = pd.date_range(start = start_date, end = start_date if end_date is None else end_date)
+    
+    for dt in date_list:
+        dt = dt.strftime('%d/%m/%Y')
+        print(f'Extraindo negociações de Swap Pré X IPCA para o dia {dt}')
+        
+        df_rate = _real(dt) # request data based on date (dt)
+        df = df_rate if not len(df) else pd.concat([df, df_rate]) # concat new data to the data frame 
+        df = df.reindex(columns=sorted(list(df.columns))) # sort columns by tenure 
+    return df 
+
+#%%# Solicitação da curva Nominal para um dia ou para um período
+
+def nominal(start_date, end_date=None): 
+
+    """
+    Parameters
+    ----------
+    start_date: "yyyy-mm-dd"
+    end_date: "yyyy-mm-dd" (optional)
+    
+        
+    Returns
+    -------
+    data: yield curve for the given date or range
+    """
+
+    df = []
+    
+    date_list = pd.date_range(start = start_date, end = start_date if end_date is None else end_date)
+    
+    for dt in date_list:
+        dt = dt.strftime('%d/%m/%Y')
+        print(f'Extraindo negociações de Swap Pré X DI para o dia {dt}')
+        
+        df_rate = _nominal(dt) # request data based on date (dt)
+        df = df_rate if not len(df) else pd.concat([df, df_rate]) # concat new data to the data frame 
+        df = df.reindex(columns=sorted(list(df.columns))) # sort columns by tenure 
+    return df
+
+#%%# Update da base de dados 
+
