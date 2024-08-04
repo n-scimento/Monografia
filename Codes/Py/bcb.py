7ab9769c489aba05d2943a5c21399178c5fd778d
n-scimento <g.nascimento.p@icloud.com>
2024-08-04 16:21:56 -0300

    starting bcb

--- Codes/Py/bcb.py
+++ Codes/Py/bcb.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+#%%# To-do
+
 """
 Created on Wed Jul 31 00:17:17 2024
 
@@ -23,7 +26,8 @@ Dados:
     SELIC
     
 Links:
-    Documentação: https://olinda.bcb.gov.br/olinda/servico/Expectativas/versao/v1/documentacao
+    Documentação: 
+    https://olinda.bcb.gov.br/olinda/servico/Expectativas/versao/v1/documentacao
     https://olinda.bcb.gov.br/olinda/servico/Expectativas/versao/v1/swagger-ui3#/
     https://dadosabertos.bcb.gov.br/dataset/expectativas-mercado/resource/dc8139ea-2555-48d7-9026-54e3b5d1815b?inner_span=True
     https://olinda.bcb.gov.br/olinda/servico/Expectativas/versao/v1/swagger-ui2#/
@@ -31,3 +35,16 @@ Links:
 
 """
 
+#%%# Libraries 
+import requests 
+import pandas as pd
+
+#%%# Code 
+url = 'olinda.bcb.gov.br/olinda/servico/Expectativas/versao/v1/odata'
+
+response = requests.get("https://olinda.bcb.gov.br/olinda/servico/Expectativas/versao/v1/odata/ExpectativasMercadoSelic")
+
+df = pd.DataFrame(response.json()['value'])
+
+
+df_pivot = pd.pivot_table(df, values = 'Mediana', columns = 'Reuniao', index = 'Data')
